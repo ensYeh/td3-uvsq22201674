ab2061550c979dac24de661b105c33fd5de3a9a0
uvsq22201674 <sama.gharib-ali-barura@ens.uvsq.fr>
2025-10-03 11:50:29 +0200

    Implémentation de toString pour DnsItem

--- src/main/java/fr/uvsq/cprog/collex/DnsItem.java
+++ src/main/java/fr/uvsq/cprog/collex/DnsItem.java
@@ -11,4 +11,12 @@ public class DnsItem {
         this.adresse = ip;
         this.nom = n;
     }
+
+    public String toString() {
+        return String.format(
+            "%s %s",
+            this.adresse.toString(),
+            this.nom.toString()
+        );
+    }
 }
