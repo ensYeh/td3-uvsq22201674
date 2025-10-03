7af2940f9699be06abdc224d9bd7039fe0093c33
uvsq22201674 <sama.gharib-ali-barura@ens.uvsq.fr>
2025-10-03 12:48:19 +0200

    Ajout d'un test (pas encore implémenté)

---
+++ src/test/java/fr/uvsq/cprog/collex/DnsTest.java
@@ -0,0 +1,12 @@
+package fr.uvsq.cprog.collex;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+public class DnsTest {
+    @Test
+    public void doubleInsertion() {
+        // TODO: Implémenter ce test
+    }
+}
