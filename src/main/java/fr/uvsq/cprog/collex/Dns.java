7647bcf9f5b7af452db4afba59c5d9f414bbc739
uvsq22201674 <sama.gharib-ali-barura@ens.uvsq.fr>
2025-10-03 14:11:16 +0200

    Ajout du test pour Dns

--- src/main/java/fr/uvsq/cprog/collex/Dns.java
+++ src/main/java/fr/uvsq/cprog/collex/Dns.java
@@ -1,5 +1,24 @@
 package fr.uvsq.cprog.collex;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
 public class Dns {
-	
+    private Properties proprietesBDD = null;
+
+    public Dns() throws IOException {
+      this.chargerProprietes();
+    }
+
+    private void chargerProprietes() throws IOException {
+      InputStream flux = Files.newInputStream(
+        Paths.get("src/main/resources/database.properties")
+      );
+
+      this.proprietesBDD = new Properties();
+      this.proprietesBDD.load(flux);
+    }
 }
--- src/test/java/fr/uvsq/cprog/collex/DnsTest.java
+++ src/test/java/fr/uvsq/cprog/collex/DnsTest.java
@@ -3,8 +3,19 @@ package fr.uvsq.cprog.collex;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import org.junit.Test;
+import java.io.IOException;
 
 public class DnsTest {
+    @Test
+    public void testConstructeur() {
+        try {
+            Dns d = new Dns();
+        } catch (IOException e) {
+            System.out.println(String.format("Erreur : %s", e.getMessage()));
+            fail();
+        }
+    }
+
     @Test
     public void doubleInsertion() {
         // TODO: Implémenter ce test
