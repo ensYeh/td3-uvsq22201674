e8c2dd004107496e0ed1828a3f24d86017b29ebb
uvsq22201674 <sama.gharib-ali-barura@ens.uvsq.fr>
2025-10-03 11:35:34 +0200

    Ajout des tests pour NomMachine

---
+++ src/test/java/fr/uvsq/cprog/collex/NomMachineTest.java
@@ -0,0 +1,22 @@
+package fr.uvsq.cprog.collex;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+import fr.uvsq.cprog.collex.FormatException;
+
+public class NomMachineTest {
+    @Test
+    public void testFormat() {
+        try {
+            NomMachine nm = new NomMachine("fr.uvsq.sama_gharib");
+        } catch(FormatException e) {
+            fail();
+        }
+        assertThrows(
+            FormatException.class,
+            () -> new NomMachine("Je suis un nom de machine invalide");
+        );
+    }
+}
