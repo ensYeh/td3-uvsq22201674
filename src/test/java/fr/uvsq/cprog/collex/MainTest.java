f0258e06809b4624cdf6a68ddbbff1f828eb7b1e
uvsq22201674 <sama.gharib-ali-barura@ens.uvsq.fr>
2025-10-03 10:33:30 +0200

    Ajout du fichier MainTest.java

---
+++ src/test/java/fr/uvsq/cprog/collex/MainTest.java
@@ -0,0 +1,11 @@
+package fr.uvsq.cprog.collex;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public final class MainTest {
+    @Test
+    public void junitImportedTest() {
+        assertTrue(true);
+    }
+}
