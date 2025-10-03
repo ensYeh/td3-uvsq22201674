23e17d41c495bffae293ab46edb2a7495f88d7a5
uvsq22201674 <sama.gharib-ali-barura@ens.uvsq.fr>
2025-10-03 11:49:39 +0200

    Implémentation du constructeur pour DnsItem

--- src/main/java/fr/uvsq/cprog/collex/DnsItem.java
+++ src/main/java/fr/uvsq/cprog/collex/DnsItem.java
@@ -1,5 +1,14 @@
 package fr.uvsq.cprog.collex;
 
+import fr.uvsq.cprog.collex.AdresseIP;
+
 public class DnsItem {
 
+    private AdresseIP adresse;
+    private NomMachine nom;
+
+    public DnsItem(AdresseIP ip, NomMachine n) {
+        this.adresse = ip;
+        this.nom = n;
+    }
 }
