31c50c1f6d10ca283288dcc526c02c3dc8fe6783
uvsq22201674 <sama.gharib-ali-barura@ens.uvsq.fr>
2025-10-08 19:56:23 +0200

    Implémentation du test pour DnsTUI

--- src/test/java/fr/uvsq/cprog/collex/DnsTUITest.java
+++ src/test/java/fr/uvsq/cprog/collex/DnsTUITest.java
@@ -1,14 +1,25 @@
 package fr.uvsq.cprog.collex;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import org.junit.Test;
 
 public class DnsTUITest {
     @Test
-    public void testParsingCommande() {
-        // TODO
+    public void testParsingCommande() throws IOException {
+        DnsTUI tui = new DnsTUI();
+    
+        Commande ajout = tui.parserCommande("add 192.168.0.1 www.uvsq.fr");
+        Commande adresse = tui.parserCommande("192.168.0.1");
+        Commande nom = tui.parserCommande("www.uvsq.fr");
+        Commande liste_nom = tui.parserCommande("ls uvsq.fr");
+        Commande liste_adresse = tui.parserCommande("ls -a uvsq.fr");
+        Commande quitter = tui.parserCommande("quit");
+
+        assertTrue(ajout instanceof AjouterItem);
+        assertTrue(adresse instanceof RechercherAdresse);
+        assertTrue(nom instanceof RechercherNom);
+        assertTrue(liste_nom instanceof ListeMachines);
+        assertTrue(liste_adresse instanceof ListeMachines);
+        assertTrue(quitter instanceof QuitterApp);
     }
 }
